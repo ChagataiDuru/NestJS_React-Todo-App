da7169cb3757290cd1eeffc3112d8c0c1b2afe96
Çağatay <durucagatay01@gmail.com>
2023-06-19 10:43:46 +0300

    Updating app addind new libraries

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe  } from '@nestjs/common';
+import { ValidationPipe,VersioningType  } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // enable cors
   app.useGlobalPipes(
-    new ValidationPipe()
+    new ValidationPipe({})
   );
+  app.enableCors({ origin: '*' })
+  app.useGlobalPipes(new ValidationPipe({}))
+  app.enableVersioning({ type: VersioningType.URI })
+  app.use(helmet())
+  app.use(compression())
   await app.listen(3000);
 }
 bootstrap();
