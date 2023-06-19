b5edcdb6caed78af2484330c1135bd136a666e4f
Çağatay <durucagatay01@gmail.com>
2023-06-19 15:14:18 +0300

    Implemented Timeout exception

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe,VersioningType  } from '@nestjs/common';
 import helmet from 'helmet';
 import * as compression from 'compression'
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({})
   );
+  app.useGlobalInterceptors(new TimeoutInterceptor());
   app.enableCors({ origin: '*' }) // enable cors for all origins
   app.useGlobalPipes(new ValidationPipe({}))
   app.enableVersioning({ type: VersioningType.URI })
